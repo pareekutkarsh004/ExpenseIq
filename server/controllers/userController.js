bf2ddb76927b40a806b6127ed2ad87faad98fd23
Utkarsh <utkarshpareek04@gmail.com>
2026-04-02 15:38:40 +0530

    Add user deletion and retrieval functionality; update user routes to include getAllUsers

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -43,9 +43,11 @@ export const updateUser = async (req, res) => {
   try {
     const { firebaseUID } = req.user;
 
-    const updatedUser = await User.findOneAndUpdate(
+    const { name } = req.body;
+
+const updatedUser = await User.findOneAndUpdate(
   { firebaseUID },
-  req.body,
+  { name },
   { returnDocument: "after" }
 );
 
@@ -59,9 +61,23 @@ export const deleteUser = async (req, res) => {
   try {
     const { firebaseUID } = req.user;
 
-    await User.findOneAndDelete({ firebaseUID });
+    const user = await User.findOneAndDelete({ firebaseUID });
+
+if (!user) {
+  return res.status(404).json({ message: "User not found" });
+}
 
     res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find().select("name email");
+
+    res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,6 +1,7 @@
 import express from 'express';  
 import { verifyFirebaseToken } from '../middlewares/firebaseAuth.middleware.js';
-import { registerOrLoginUser,deleteUser,getCurrentUser,updateUser } from '../controllers/userController.js';
+import { registerOrLoginUser,deleteUser,getCurrentUser,updateUser,getAllUsers } from '../controllers/userController.js';
+
 const router = express.Router();
 
 // import { verifyFirebaseToken } from '../middlewares/firebaseAuth.middleware.js';
@@ -9,5 +10,5 @@ router.post('/register-or-login', verifyFirebaseToken, registerOrLoginUser);
 router.get('/current-user', verifyFirebaseToken, getCurrentUser);
 router.put('/update-user', verifyFirebaseToken, updateUser);
 router.delete('/delete-user', verifyFirebaseToken, deleteUser);
-
+router.get('/all-users', verifyFirebaseToken, getAllUsers);
 export default router;
