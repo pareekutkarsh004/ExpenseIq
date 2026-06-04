1f202363bfe1d75973244299b7c6c9afe7a95671
Utkarsh <utkarshpareek04@gmail.com>
2026-06-04 12:50:05 +0530

    feat: support loading firebase credentials from env variable on Render

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -1,9 +1,21 @@
 import admin from "firebase-admin";
 import fs from "fs";
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync(new URL("./serviceAccountKey.json", import.meta.url))
-);
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  try {
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+  } catch (error) {
+    console.error("Failed to parse FIREBASE_SERVICE_ACCOUNT env variable:", error);
+  }
+}
+
+if (!serviceAccount) {
+  serviceAccount = JSON.parse(
+    fs.readFileSync(new URL("./serviceAccountKey.json", import.meta.url))
+  );
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
